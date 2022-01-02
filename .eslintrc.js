56c3816c636b2e357c6419f8ae711a8039626624
Fred <castelloiv@gmail.com>
2022-01-02 02:13:52 -0500

    Prettier eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    parser: "@typescript-eslint",
+    extends: [
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@/typescript-eslint",
+        "plugin:prettier/recommended",
+    ]
+}
