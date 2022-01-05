dcf2286150a96b7bd9b84a1aab7cbfce373a931e
Fred <castelloiv@gmail.com>
2022-01-04 22:20:24 -0500

    Updating

---
+++ src/middleware/check.middleware.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction, RequestHandler } from 'express';
+import Joi from 'joi';
+
+function validationMiddleware(schema: Joi.Schema): RequestHandler {
+    return async (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> => {
+        const validationoptions = {
+            abortEarly: false,
+            allowUnknown: true,
+            stripUnknown: true,
+        };
+        try {
+            const value = await schema.validateAsync(
+                req.body,
+                validationoptions
+            );
+            req.body = value;
+            next();
+        } catch (e) {
+            const errors: string[] = [];
+            e.details.forEach((error: Joi.ValidationErrorItem) => {
+                errors.push(error.message);
+            });
+            res.status(400).send({ errors });
+        }
+    };
+}
